45f05f808f6d47587d7809066452c2dab8c2edc9
Elon <elon@quotabook.com>
2022-10-23 02:54:20 +0900

    Initialize commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  useTabs: false,
+};
