f2040b5b69dca23e2ba0818ea491b047d8d1e60c
jungpaeng <meis1541@naver.com>
2022-10-23 14:11:22 +0900

    chore(renderer): esbuild bundler settings

---
+++ packages/renderer/esbuild.config.js
@@ -0,0 +1,28 @@
+const { build } = require('esbuild');
+
+const pkg = require('./package.json');
+
+const watch = process.argv.includes('--watch');
+const external = Object.keys({
+  ...pkg.dependencies,
+  ...pkg.peerDependencies,
+});
+
+const base = ({ entryPoints = ['./src/index.ts'], outdir = 'dist' }) => ({
+  entryPoints,
+  outdir,
+  external,
+  target: 'es2015',
+  watch,
+  minify: !watch,
+  sourcemap: !watch,
+});
+
+Promise.all([
+  build({ ...base({}), format: 'cjs' }),
+  build({
+    ...base({}),
+    format: 'esm',
+    outExtension: { '.js': '.mjs' },
+  }),
+]).catch(() => process.exit(1));
