ec91ebf4f61d434b02d9900f01554cfd58f8175a
jungpaeng <meis1541@naver.com>
2022-10-23 13:55:31 +0900

    chore(renderer): dts-bundle-generator

--- .pnp.cjs
+++ .pnp.cjs
@@ -2506,6 +2506,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@form-builder/renderer", "workspace:packages/renderer"],\
             ["@types/react", "npm:18.0.21"],\
             ["@typescript-eslint/eslint-plugin", "virtual:f02a8ecf3109d1ab405cd5f84b24b445f27d363d08f3e17c039300aa8f42f857d5300025f4d0e707f7227217f2f025863a19d12e97af7832b99746a7a03ebf77#npm:5.40.1"],\
+            ["dts-bundle-generator", "npm:7.0.0"],\
             ["esbuild", "npm:0.15.12"],\
             ["eslint-plugin-import", "virtual:f02a8ecf3109d1ab405cd5f84b24b445f27d363d08f3e17c039300aa8f42f857d5300025f4d0e707f7227217f2f025863a19d12e97af7832b99746a7a03ebf77#npm:2.26.0"],\
             ["eslint-plugin-prettier", "virtual:f02a8ecf3109d1ab405cd5f84b24b445f27d363d08f3e17c039300aa8f42f857d5300025f4d0e707f7227217f2f025863a19d12e97af7832b99746a7a03ebf77#npm:4.2.1"],\
@@ -4351,6 +4352,17 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/dotenv-npm-10.0.0-36b272df25-f412c5fe8c.zip/node_modules/dotenv/",\
           "packageDependencies": [\
             ["dotenv", "npm:10.0.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["dts-bundle-generator", [\
+        ["npm:7.0.0", {\
+          "packageLocation": "./.yarn/cache/dts-bundle-generator-npm-7.0.0-451d9c7b76-e48a70ae66.zip/node_modules/dts-bundle-generator/",\
+          "packageDependencies": [\
+            ["dts-bundle-generator", "npm:7.0.0"],\
+            ["typescript", "patch:typescript@npm%3A4.8.4#~builtin<compat/typescript>::version=4.8.4&hash=701156"],\
+            ["yargs", "npm:17.6.0"]\
           ],\
           "linkType": "HARD"\
         }]\
